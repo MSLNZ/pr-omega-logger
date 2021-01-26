107d95cf47ce40f9312c33eb3b3703f9dcc6c46b
jborbely <joe.borbely@gmail.com>
2021-01-27 11:56:41 +1300

    use record.alias instead of record.user_defined['usual_location'] for the label

--- omega_logger/webapp.py
+++ omega_logger/webapp.py
@@ -55,9 +55,9 @@ dropdown_options = list()
 calibrations = dict()
 cfg = Config(sys.argv[1])
 omegas = cfg.database().records(manufacturer='OMEGA')
-for omega in sorted(omegas, key=lambda r: r.user_defined['usual_location']):
+for omega in sorted(omegas, key=lambda r: r.alias):
     dropdown_options.append({
-        'label': omega.user_defined['usual_location'] + ' - ' + omega.serial,
+        'label': omega.alias + ' - ' + omega.serial,
         'value': omega.model + '_' + omega.serial + '.sqlite3'
     })
     element = cfg.root.find('.//omega[@serial="%s"]' % omega.serial)
