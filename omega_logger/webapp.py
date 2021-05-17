1352bc03d986ff0fd757a8fee7584309a281b7bb
rebeccahawke <rebecca.hawke@measurement.govt.nz>
2021-05-17 13:25:42 +1200

    modified current readings view for two sensors per logger
    Displays temperature, humidity, and dewpoint for sensors 1 and 2 on separate lines.

--- omega_logger/webapp.py
+++ omega_logger/webapp.py
@@ -490,6 +490,13 @@ def current_readings_viewer(tab, n_intervals):
         children.append(html.B(b))
         if data['error']:
             children.append(html.P(data['error']))
+        elif 'temperature2' in data.keys():
+            for sensor in ['1', '2']:
+                p = []
+                for key in 'temperature' + sensor, 'humidity' + sensor, 'dewpoint' + sensor:
+                    p.append(html.Span(key + ':', style={'color': 'grey'}))
+                    p.append(html.Span(f'{data[key]:.2f}', style={'margin-right': margin_right}))
+                children.append(html.P(p))
         else:
             p = []
             for key in sorted(data):
