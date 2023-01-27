71c07f3b7e3509af838b0920da2fc686678b9ca3
jborbely <joe.borbely@gmail.com>
2023-01-27 15:18:19 +1300

    replace _ with - in sphinx-rtd-theme

--- setup.py
+++ setup.py
@@ -156,7 +156,7 @@ install_requires = [
 
 tests_require = ['pytest', 'pytest-cov', 'requests']
 
-docs_require = ['sphinx', 'sphinx_rtd_theme']
+docs_require = ['sphinx', 'sphinx-rtd-theme']
 
 testing = {'test', 'tests', 'pytest'}.intersection(sys.argv)
 pytest_runner = ['pytest-runner'] if testing else []
